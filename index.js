abf2b1090b1940830b1536e0d922f64f618c8ce8
Ullavs <ullavansprang@gmail.com>
2022-01-30 18:04:23 +0100

    review with Rob

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".navbar");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
+hamburger.addEventListener("click", () => {
   navBar.classList.toggle("active");
-}
+});
 
-navMenu.addEventListener("click", removeMenu);
-
-function removeMenu(event) {
+navMenu.addEventListener("click", (event) => {
   if (event.target.classList.contains("nav-link")) {
     navBar.classList.remove("active");
   }
-}
+});
